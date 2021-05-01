febd9b79ad9fd591ad0af033064017f6e03c89c4
Ruby Ann Matias <rubyann.y.matias@gmail.com>
2021-05-01 22:54:11 +0800

    project init

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+  apiKey: "AIzaSyBIQRh7Ag10GpGr5NAwEclrkj8hqMiTsyw",
+  authDomain: "equipment-audit-d939d.firebaseapp.com",
+  projectId: "equipment-audit-d939d",
+  storageBucket: "equipment-audit-d939d.appspot.com",
+  messagingSenderId: "195422472625",
+  appId: "1:195422472625:web:5abc58763b02e76eab9590",
+  measurementId: "G-E0SXGDQXVQ"
+  }
 };
 
 /*
@@ -14,3 +23,5 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
