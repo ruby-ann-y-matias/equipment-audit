1904abbb4fde06ba5dad8f4bb29fa95f94b5e7e5
Gie Basinal <giebasinal@gmail.com>
2021-05-16 21:25:27 +0800

    converted stylesheet to scss, added login page structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { FullComponent } from './layouts/full/full.component';
+import { LoginComponent } from './pages/authentication/login/login.component';
 
 export const Approutes: Routes = [
   {
@@ -21,18 +22,7 @@ export const Approutes: Routes = [
   },
   {
     path: 'login' , pathMatch: 'full' ,
-    component: FullComponent,
-    children: [
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-      {
-        path: 'dashboard',
-        loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
-      },
-      {
-        path: 'component',
-        loadChildren: () => import('./component/component.module').then(m => m.ComponentsModule)
-      }
-    ]
+    component: LoginComponent
   },
   {
     path: '**',
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import firebase from 'firebase/app';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'equipment-audit';
